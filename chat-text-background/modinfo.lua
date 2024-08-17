249b1771256fd9100e88a4b9c5bac2fee7ba3e72
gibberish <gbjgms@gmail.com>
2024-08-17 14:15:32 +0700

    Added mod scripts

---
+++ chat-text-background/modinfo.lua
@@ -0,0 +1,38 @@
+name = "Chat Text Background"
+description = "Adds a transparent dark background behind chat text for better readability."
+author = "gibberish"
+version = "1.0"
+api_version = 10
+
+dst_compatible = false
+forge_compatible = false
+gorge_compatible = false
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+
+client_only_mod = true
+all_clients_require_mod = false
+
+configuration_options = {
+    {
+		name = "bg_opacity",
+		label = "Background Opacity",
+		hover = "Adjusts how opaque/transparent the chat background looks.",
+		options =	
+		{
+			{description = "0%", data = 1},
+			{description = "10%", data = 2},
+			{description = "20%", data = 3},
+			{description = "30%", data = 4},
+			{description = "40%", data = 5},
+            {description = "50%", data = 6},
+            {description = "60%", data = 7},
+            {description = "70%", data = 8},
+            {description = "80%", data = 9},
+            {description = "90%", data = 10},
+            {description = "100%", data = 11},
+		},
+		default = 5,
+	},
+}
---
+++ chat-text-background/modmain.lua
@@ -0,0 +1,60 @@
+local Image = require "widgets.image"
+
+local bg_opacity_enum = {
+    [1] = 0,
+    [2] = 0.1,
+    [3] = 0.2,
+    [4] = 0.3,
+    [5] = 0.4,
+    [6] = 0.5,
+    [7] = 0.6,
+    [8] = 0.7,
+    [9] = 0.8,
+    [10] = 0.9,
+    [11] = 1,
+}
+local bg_opacity = bg_opacity_enum[GetModConfigData("bg_opacity", true)]
+
+AddClassPostConstruct("screens.playerhud", function(self)
+    self.chat_history_background = self.chat_history_label:AddChild(Image("images/global/square.tex"))
+        :SetName("Chat Background")
+        :SetMultColor(GLOBAL.UICOLORS.BACKGROUND_DARK)
+        :SetMultColorAlpha(bg_opacity) -- adjust this to change the opacity
+        :SendToBack() -- put the background behind chat text
+        :Hide()
+
+    local original_ShowChatHistory = self.ShowChatHistory
+    self.ShowChatHistory = function()
+        print("begin debugging")
+        if GetModConfigData ~= nil then
+            print("GetModConfigData exists")
+
+            if GetModConfigData("bg_opacity", true) ~= nil then
+                print("GetModConfigData bg_opacity true exists")
+
+                print(GetModConfigData("bg_opacity", true))
+            end
+        end
+        print("end debugging")
+
+        original_ShowChatHistory(self)
+
+        if GLOBAL.TheDungeon and GLOBAL.TheDungeon.components.chathistory and GLOBAL.next(GLOBAL.TheDungeon.components.chathistory:GetHistory()) == nil then
+            -- dont show the background when theres no text yet, since theres
+            -- no bounding box and it would just be a small, empty rectangle
+            return
+        end
+
+        local x1, y1, x2, y2 = self.chat_history_label:GetBoundingBox()
+        local w, h = x2 - x1, y2 - y1
+
+        -- add padding
+        w = w + 40
+        h = h + 40
+
+        self.chat_history_background
+            :SetRect(x1, y1, w, h) -- sets the background's size to the chat text bounding box
+            :SetPosition(0, 0) -- resets the background to the center of chat text
+            :Show()
+    end
+end)
